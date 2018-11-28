2c43f8e9ca1224c16bc5a390ff74b5ce58d35aff
Ini Akinleye <inioluwaakinleye@gmail.com>
2018-11-28 11:22:06 +0100

    modify string compression code

--- C++Practice/StringCompression.cpp
+++ C++Practice/StringCompression.cpp
@@ -6,37 +6,40 @@ using namespace std;
 string compression(const string&);
 
 int main() {
-    string string1;
+    string string1, CompString;
     cout << "Enter string for compression" << endl;
-    cin >> string1;
-
-    cout << "The compressed string is " << compression(string1);
+    getline(cin, string1);
+    CompString = compression(string1);
+    if (!string1.empty()) {
+        if (string1 == CompString)
+            cout << "The string remains " << CompString << endl;
+        else
+            cout << "The compressed string is " << CompString;
+    }
 }
 
 string compression(const string& s) {
     string ret;
-    if (s.size() == 0) {
+    if (s.empty()) {
         cout << "Empty string" << endl;
     }
     else {
-        if (s.size() >= 1 && ret.size() < s.size()) {
-            int n = s.size();
+        int n = s.size();
 
-            ret[0] = s[0];
-
-            for (int i = 0; i < n; i++) {
-                int temp = 1;
-                while (i < n - 1 && s[i] == s[i + 1]) {
-                    temp++;
-                    i++;
-                }
+        for (int i = 0; i < n; i++) {
+            int temp = 1;
+            while (i < n - 1 && s[i] == s[i + 1]) {
+                temp++;
+                i++;
+            }
+            if (temp == 1) {
+                ret.push_back(s[i]);
+            }
+            else {
                 ret.push_back(s[i]);
                 ret.push_back(static_cast<char>('0' + temp));
             }
-            return ret;
-        }
-        else {
-            return s;
         }
     }
+    return ret;
 }
