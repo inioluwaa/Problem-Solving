0d53a9ae528db75513ed35777b94344d8b415402
Ini Akinleye <inioluwaakinleye@gmail.com>
2019-04-04 15:54:24 +0100

    add ReverseWordAndEquation.cpp and LookAndSay.cpp files

---
+++ DSA/Strings/LookAndSay.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+string LookAndSay(int);
+string NextNumber(const string &);
+
+int main() {
+    cout << LookAndSay(8);
+}
+
+string LookAndSay(int n) {
+    string s = "1";
+    for (int i{1}; i < n; ++i) {
+        s = NextNumber(s);
+    }
+    return s;
+}
+
+string NextNumber(const string &s) {
+    string result;
+    for (int i{0}; i < s.size(); ++i) {
+        int count = 1;
+        while (i + 1 < s.size() && s[i] == s[i + 1]) {
+            ++i, ++count;
+        }
+        result += to_string(count) + s[i];
+    }
+    return result;
+}
--- DSA/Strings/ReversesWordAndEquation.cpp
+++ DSA/Strings/ReversesWordAndEquation.cpp
@@ -11,6 +11,8 @@ int main() {
     cout << *EquPtr;
 }
 
+// Reverse all the words in a sentence... Alice likes Bob -> Bob likes Alice.
+// Using Pointers.
 void ReverseWords(string *sPtr) {
     // Reverses the whole string first.
     reverse(sPtr->begin(), sPtr->end());
@@ -25,6 +27,8 @@ void ReverseWords(string *sPtr) {
     reverse(sPtr->begin() + start, sPtr->end());
 }
 
+// Reverses all the numbers in an equation.. 100+400-300 -> 300-400+100
+// Using Pointers.
 void ReverseEquation(string *EquPtr) {
     // Reverses the whole string first.
     reverse(EquPtr->begin(), EquPtr->end());
@@ -37,4 +41,20 @@ void ReverseEquation(string *EquPtr) {
     }
     // Reverses the last word.
     reverse(EquPtr->begin() + start, EquPtr->end());
+}
+
+// Reverses all the numbers in an equation.. 100+400-300 -> 300-400+100
+// Using References.
+void ReverseEquation(string &EquPtr) {
+    // Reverses the whole string first.
+    reverse(EquPtr.begin(), EquPtr.end());
+    size_t start = 0, end;
+
+    while ((end = EquPtr.find_first_of("+-*/", start)) != string::npos) {
+        // Reverses each word in the string but not the last word.
+        reverse(EquPtr.begin() + start, EquPtr.begin() + end);
+        start = end + 1;
+    }
+    // Reverses the last word.
+    reverse(EquPtr.begin() + start, EquPtr.end());
 }
