cafb7e41025ec0aac81ff30a73989026fe56b04e
Ini Akinleye <inioluwaakinleye@gmail.com>
2019-04-05 02:01:14 +0100

    add RomanToInteger.cpp file

--- DSA/Strings/RomanToInteger.cpp
+++ DSA/Strings/RomanToInteger.cpp
@@ -3,16 +3,18 @@
 #include <unordered_map>
 
 using namespace std;
+int RomanToInteger(const string &);
 
 int main() {
+    cout << RomanToInteger("IX");
 }
-
-int RomanToInteger(const string& s) {
-    unordered_map<char, int> T = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
-                                  {'C', 100}, {'D', 500}, {'M', 500}};
+// LIX
+int RomanToInteger(const string &s) {
+    unordered_map<char, int> T = { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+                                   {'C', 100}, {'D', 500}, {'M', 1000} };
 
     int sum = T[s.back()];
-    for (int i = s.length(); i >= 0; --i) {
+    for (int i = s.size() - 2; i >= 0; --i) {
         if (T[s[i]] < T[s[i + 1]]) {
             sum -= T[s[i]];
         }
