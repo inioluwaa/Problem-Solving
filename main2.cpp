fb25e525a5ea58f4effe2808a85e7d6b9fedeb43
Ini Akinleye <inioluwaakinleye@gmail.com>
2019-05-20 09:14:18 +0100

    add addup problem

--- main2.cpp
+++ main2.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
+using namespace std;
+bool CheckKey(unordered_set<int> u, int key);
+bool AddUp(vector<int>, int);
 
+int main() {
+    vector<int> u = {10, 15, 3, 7};
+    bool answer = AddUp(u, 23);
+    cout << answer;
+}
+
+bool AddUp(vector<int> u, int target) {
+    unordered_set<int> Checkers {};
+    if (u.empty()) return false;
+    for(int curr : u) {
+        if(CheckKey(Checkers, target - curr)) return true;
+        Checkers.insert(curr);
+    }
+    return false;
+}
+
+bool CheckKey(unordered_set<int> u, int key) {
+    return !(u.find(key) == u.end());
+}
